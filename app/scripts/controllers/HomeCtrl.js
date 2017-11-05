21fc707378ac09c2ebc7f7b5c4e3784b6b86c60f
Andre Dean <Andre@Andres-Retina-MacBook-Pro.local>
2017-11-04 22:34:28 -0700

    Add functionality to send messages

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,17 +1,26 @@
 (function() {
-  function HomeCtrl(Room, Message) {
+  function HomeCtrl(Room, Message,$cookies) {
     this.rooms = Room.all;
     this.roomName = '';
+    this.SendMessage = {};
     this.message = {};
+    this.SendMessage.userName = $cookies.get('blocChatCurrentUser');
 
     this.activeRoom = function(room){
       this.roomName = room.name;
+      this.SendMessage.roomId = room.$id;
       this.message = Message.getByRoomId(room.$id);
     };
+
+    this.sendMessage = function() {
+      this.SendMessage.sentAt = new Date().toLocaleString();
+      Message.send(this.SendMessage);
+      this.SendMessage.content = '';
+    }
 
   }
 
   angular
     .module('blocChat')
-    .controller('HomeCtrl', ['Room', 'Message', HomeCtrl]);
+    .controller('HomeCtrl', ['Room', 'Message','$cookies', HomeCtrl]);
 })();
--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -2,9 +2,14 @@
   function Message($firebaseArray){
     var Message = {};
     var ref = firebase.database().ref().child("messages");
+    var message = $firebaseArray(ref);
 
     Message.getByRoomId = function(roomId){
       return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
+    };
+
+    Message.send = function(newMessage) {
+      message.$add(newMessage);
     };
 
     return Message;
